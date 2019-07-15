f2ab731844d20731f7de473b10b38b978cd10ff0
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-15 00:01:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
-def key_for_min_value(name_hash)
-
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+def key_for_min_value(hash)
+  lowest_key = nil 
+  lowest_value = nil
+  
+  hash.each do |key, value|
+  if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key 
+    end 
+  end
+  lowest_key
 end
+
--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -49,11 +49,11 @@ describe "smallest hash value" do
   end
 
   it "returns the key of the smallest hash value" do
-    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
+    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam => 1)
   end
 
   it "returns the key of the smallest hash value example 2" do
-    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake)
+    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake => 10)
   end
 
   it "returns nil for an empty hash" do
